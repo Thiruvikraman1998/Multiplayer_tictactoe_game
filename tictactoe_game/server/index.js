0b30ad2bc3487f50ac5507f6b18e15c7150e162e
Thiruvikraman1998 <development.thiru@gmail.com>
2023-07-12 22:56:54 +0530

    node.js server created

---
+++ tictactoe_game/server/index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const http = require("http");
+
+// initialize and store express in app 
+const app = express();
+
+const port = process.env.PORT || 3000;  // once server deployed to web it runs with the port that has using process.env.PORT, if not deployed it runs with port 3000
+
+var server = http.createServer(app);  // creates a server, here createServer() takes and argument requestListner, here app is that listener
+
+var socketio = require("socket.io");
+var io = socketio(server);  
+// short hand code for above two lines is: var io = require("socket.io")(server);
+
+
+//middle ware, where the data from client goes to the middle ware and converts to json or do some functions and reaches to server.
+// here we are converting data to json using middle ware 
+app.use(express.json());
+
+server.listen(port, "0.0.0.0", function(){
+console.log(`Server started and running in port ${port}`);  // in javascript the string interpolation can be done using backtciks(``), rather than normal single or double quotes.
+})
