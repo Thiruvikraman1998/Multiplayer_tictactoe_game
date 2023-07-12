92688caa1d9ac9e744183a05a33e1a99120b0e77
Thiruvikraman1998 <development.thiru@gmail.com>
2023-07-12 19:05:50 +0530

    completed join room UI

--- tictactoe_game/lib/screens/join_room_screen.dart
+++ tictactoe_game/lib/screens/join_room_screen.dart
@@ -1,11 +1,55 @@
 import 'package:flutter/material.dart';
 
-class JoinRoom extends StatelessWidget {
+import '../responsive/responsive.dart';
+import '../widgets/buttons/main_menu_buttons.dart';
+import '../widgets/custom_text.dart';
+import '../widgets/custom_textfield.dart';
+
+class JoinRoom extends StatefulWidget {
   static String routeName = '/join_room';
   const JoinRoom({super.key});
 
   @override
+  State<JoinRoom> createState() => _JoinRoomState();
+}
+
+class _JoinRoomState extends State<JoinRoom> {
+  final TextEditingController _nameController = TextEditingController();
+  final TextEditingController _gameIdController = TextEditingController();
+
+  @override
+  void dispose() {
+    _nameController.dispose();
+    _gameIdController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    final size = MediaQuery.of(context).size;
+    return Scaffold(
+      body: Responsive(
+        child: Container(
+          margin: const EdgeInsets.symmetric(horizontal: 20),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const CustomText(
+                  shadows: [Shadow(blurRadius: 40, color: Colors.amber)],
+                  text: 'Join Room',
+                  fontSize: 80),
+              SizedBox(height: size.height * 0.05),
+              CustomTextField(
+                  hintText: 'Enter your name', controller: _nameController),
+              SizedBox(height: size.height * 0.03),
+              CustomTextField(
+                  hintText: 'Enter Game Id', controller: _gameIdController),
+              SizedBox(height: size.height * 0.05),
+              MainMenuButton(lable: 'Join', onPressedEvent: () {})
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
