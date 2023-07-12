7e90075b4c1f0888f4c7ae9f2c0cbd63e667757a
Thiruvikraman1998 <development.thiru@gmail.com>
2023-07-12 23:38:30 +0530

    socket_client for client side class created

--- tictactoe_game/server/index.js
+++ tictactoe_game/server/index.js
@@ -11,23 +11,27 @@ const port = process.env.PORT || 3000;  // once server deployed to web it runs w
 
 var server = http.createServer(app);  // creates a server, here createServer() takes and argument requestListner, here app is that listener
 
-var socketio = require("socket.io");
-var io = socketio(server);  
-// short hand code for above two lines is: var io = require("socket.io")(server);
+// var socketio = require("socket.io");
+// var io = socketio(server);  
+// short hand code for above two lines is: 
+var io = require("socket.io")(server);
+
+//middle ware, where the data from client goes to the middle ware and converts to json or do some functions and reaches to server.
+// here we are converting data to json using middle ware 
+app.use(express.json());
 
 //connect db
 const DB = "mongodb+srv://developmentthiru:lvczDhVlKywXyXWO@cluster0.vuh5yyg.mongodb.net/?retryWrites=true&w=majority";
+
+io.on("connection", (socket) =>{
+    console.log("connected");
+})
 
 mongoose.connect(DB).then(()=>{
     console.log("Connection successful");
 }).catch((e)=>{
     console.log(e);
 });
-
-
-//middle ware, where the data from client goes to the middle ware and converts to json or do some functions and reaches to server.
-// here we are converting data to json using middle ware 
-app.use(express.json());
 
 server.listen(port, "0.0.0.0", function(){
 console.log(`Server started and running in port ${port}`);  // in javascript the string interpolation can be done using backtciks(``), rather than normal single or double quotes.
